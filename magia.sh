ebc90a009028bf39efe75be5b81966d06108ead2
midiam1 <admhimmeros@gmail.com>
2021-12-05 02:53:09 -0400

    Create magia.sh
    Instalación de la criptomoneda ArepaCoin junto con las librerías de soporte para el compilado .

---
+++ magia.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+
+# Script para instalar ArepaCoin
+clear # Limpio pues no soy cochino
+# Actualizo todo
+sudo apt-et update -y
+sudo apt-get upgrade -y
+
+# Las librerías y soporte para QT
+
+ sudo apt-get install build-essential -y
+ sudo apt-get install libssl-dev -y
+ sudo apt-get install libdb++-dev -y
+ sudo apt-get install libboost-all-dev -y
+ sudo apt-get install pkg-config libcurl-openssl1.0-dev libjansson-dev libgmp-dev make g++ gcc -y
+ sudo apt-get install libminiupnpc-dev automake autoconf -y
+ sudo apt-get install libqrencode-dev -y
+
+ sudo apt install qt5-default  -y
+ sudo apt install qt4-default  -y
+
+# Descargo los binarios de ArepaCoin
+sudo apt-get install git # Por si no está instalado
+git clone https://github.com/ProjectArepa/Arepacoin
+
+cd Arepacoin
