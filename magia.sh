0962988a8b6105261576c3de63646804aff1e9d2
midiam1 <admhimmeros@gmail.com>
2021-12-06 00:01:07 -0400

    Update magia.sh

--- magia.sh
+++ magia.sh
@@ -20,6 +20,11 @@ clear # Limpio pues no soy cochino
  sudo apt install qt5-default  -y
  sudo apt install qt4-default  -y
 
+ echo "Installing Berkeley DB"
+ sudo env LC_ALL=C.UTF-8 add-apt-repository -y ppa:bitcoin/bitcoin
+ sudo apt-get -y update
+ sudo apt-get -y install libdb4.8-dev libdb4.8++-dev
+
 # Descargo los binarios de ArepaCoin
 sudo apt-get install git # Por si no está instalado
 git clone https://github.com/ProjectArepa/Arepacoin
