39461fa8a86ce92b4887eca2a46d5251c6f60550
midiam1 <admhimmeros@gmail.com>
2022-01-01 21:06:25 -0400

    +

--- readme.php
+++ readme.php
@@ -15,7 +15,7 @@
     <td valign="top"><p><strong>Arepacoin</strong> nace el 25 de septiembre del 2016 con el fin de convertirse en una moneda referente de Venezuela en el mundo, con una emisión total de 60.000.000 de Arepas y un PoS del 3% de interés anual.</p>
     <p> Está desarrollada con el algoritmo <strong>Scrypt PoW-PoS</strong>, que le permite ser una moneda robusta con un crecimiento de forma orgánica y con gran solidez.</p>
     <p>Puerto RPC: 8586 y Puerto P2P: 8585.</p>
-    <p>Los bloques minables mediante PoW tienen una recompensa generada de la siguiente manera:</p>
+    <p>Los bloques minables mediante PoW Recompensa generada de la siguiente manera:</p>
     <table border="0" cellpadding="2" cellspacing="2">
       <tr>
         <td><ul>
@@ -24,7 +24,7 @@
         <td align="right" valign="middle">1</td>
         <td valign="middle">hasta</td>
         <td align="right" valign="middle">10.000</td>
-        <td valign="middle">tiene una recompensa de 325 Arepas</td>
+        <td valign="middle">Recompensa de 325 Arepas</td>
       </tr>
       <tr>
         <td><ul>
@@ -33,7 +33,7 @@
         <td align="right" valign="middle">10.001</td>
         <td valign="middle">hasta</td>
         <td align="right" valign="middle">250.000</td>
-        <td valign="middle">tienen una recompensa de 50 Arepas</td>
+        <td valign="middle">Recompensa de 50 Arepas</td>
       </tr>
       <tr>
         <td><ul>
@@ -42,7 +42,7 @@
         <td align="right" valign="middle">250.001</td>
         <td valign="middle">hasta</td>
         <td align="right" valign="middle">500.000</td>
-        <td valign="middle">tienen una recompensa de 25 Arepas</td>
+        <td valign="middle">Recompensa de 25 Arepas</td>
       </tr>
       <tr>
         <td><ul>
@@ -51,7 +51,7 @@
         <td align="right" valign="middle">500.001</td>
         <td valign="middle">hasta</td>
         <td align="right" valign="middle">700.000</td>
-        <td valign="middle">tienen una recompensa de 12 Arepas</td>
+        <td valign="middle">Recompensa de 12 Arepas</td>
       </tr>
       <tr>
         <td><ul>
@@ -60,7 +60,7 @@
         <td align="right" valign="middle">700.001</td>
         <td valign="middle">hasta</td>
         <td align="right" valign="middle">1.000.000</td>
-        <td valign="middle">tienen una recompensa de 6 Arepas</td>
+        <td valign="middle">Recompensa de 6 Arepas</td>
       </tr>
       <tr>
         <td><ul>
@@ -69,7 +69,7 @@
         <td align="right" valign="middle">1.000.001</td>
         <td valign="middle">hasta</td>
         <td align="right" valign="middle">2.000.000</td>
-        <td valign="middle">tienen una recompensa de 3 Arepas</td>
+        <td valign="middle">Recompensa de 3 Arepas</td>
       </tr>
       <tr>
         <td><ul>
@@ -77,10 +77,10 @@
         </ul></td>
         <td align="right" valign="middle">2.000.001</td>
         <td colspan="2" valign="middle">en adelante</td>
-        <td valign="middle">tienen una recompensa de 1 Arepa</td>
+        <td valign="middle">Recompensa de 1 Arepa</td>
       </tr>
     </table>
-    <table width="100%" border="0" cellpadding="2" cellspacing="2">
+    <table width="84%" border="0" cellpadding="2" cellspacing="2">
       <tr>
         <td><strong>Enlaces</strong></td>
         <td>&nbsp;</td>
